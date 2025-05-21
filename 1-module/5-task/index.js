07c7b279ae0c45d289b163622fbc4ceac60259fe
Jenya <Ocupuc123@mail.ru>
2025-05-21 19:14:22 +0300

    5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let result = '';
+
+  if (str.length >= maxlength) {
+    result = str.slice(0, maxlength - 1) + '…';
+  } else {
+    result = str;
+  }
+
+  return result;
 }
